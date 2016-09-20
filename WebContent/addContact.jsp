c99725530f7989b81e27c01dbf8a7d1ad4687df5
JulienMrgrd <julien.margarido@gmail.com>
2016-09-20 22:15:49 +0200

    Premier commit TP1 (à finir)

---
+++ WebContent/addContact.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Add contact</title>
+</head>
+<body>
+
+	<form method=post action="LoginServlet">
+		First Name : <input type="text" name="firstname"><br>
+		Last Name : <input type="text" name="lastname"><br>
+		emailC : <input type="text" name="email"><br>
+	
+		<br/>
+		
+		<input class="button" type="submit" value="submit">
+		<input class="button" type="reset" value="reset">
+	
+	</form>
+	
+</body>
+</html>
---
+++ src/servlets/LoginServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContactServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		
+		if(name.equals(password)){
+			RequestDispatcher dispatcher = request.getRequestDispatcher("accueil.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			response.sendRedirect("index.html");
+		}
+	}
+
+}
